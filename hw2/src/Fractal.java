02692651e785782a6cebb7d63cabcd7ac70358e0
chromevoid <chromevoid500@gmail.com>
2017-11-07 08:57:57 -0500

    complete gen(frac, N) function

---
+++ hw2/src/Fractal.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Created by chrome on 11/7/17.
+ */
+
+public class Fractal {
+    public static void main(String[] args) {
+        List<Integer> outvec = gen(0.5, 8);
+        System.out.println(outvec);
+        gen(0.3, 70000);
+    }
+
+    static public List<Integer> gen(double fractal, int N) {
+        List<Integer> p = new ArrayList<Integer>();
+        for (int i = 1; i <= N; i++) {
+            p.add(i);
+        }
+        java.util.Collections.shuffle(p);
+        List<Integer> outvec = p;
+        while (p.size() > 1) {
+            p = new ArrayList<Integer>(p.subList(0, (int)(fractal * p.size())));
+            outvec.addAll(p);
+        }
+        java.util.Collections.shuffle(outvec);
+        return outvec;
+    }
+}
