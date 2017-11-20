ba3cf0ab3d1880cce1560b6ca63f18480d74c662
chromevoid <chromevoid500@gmail.com>
2017-11-20 16:31:28 -0500

    Update Q2 04:31pm

--- hw2/Q2-postgresql.sql
+++ hw2/Q2-postgresql.sql
@@ -22,7 +22,7 @@ CREATE INDEX idx_stock ON tradeIndex(stock);
 CREATE INDEX clus_idx_price ON tradeCluster(price);
 CREATE INDEX clus_idx_stock ON tradeCluster(stock);
 CLUSTER tradeCluster USING clus_idx_price;
-CLUSTER tradeCluster USING clus_idx_stock;
+CLUSTER tradeCluster USING clus_idx_stock; ### using this stock
 
 DROP INDEX idx_price;
 DROP INDEX idx_stock;
@@ -35,25 +35,39 @@ DROP INDEX idx_stock;
 
 ### query to examine the index and cluster index for price
 SELECT price from trade where price >= 300;
+### Time: 5247.077 ms
 SELECT price from tradeIndex where price >= 300;
+### Time: 4754.951 ms
 SELECT price from tradeCluster where price >= 300;
+### Time: 4396.088 ms
 
 SELECT price from trade where price = 300;
+### Time: 5158.014 ms
 SELECT price from tradeIndex where price = 300;
+### Time: 545.589 ms
 SELECT price from tradeCluster where price = 300;
+### Time: 39.335 ms
 
 ### query to examine the index and cluster index for stock
 SELECT stock from trade where stock >= 40000;
+### Time: 4537.874 ms
 SELECT stock from tradeIndex where stock >= 40000;
+### Time: 4281.204 ms
 SELECT stock from tradeCluster where stock >= 40000;
+### Time: 4456.226 ms
 
 SELECT stock from trade where stock = 40000;
+### Time: 1694.841 ms
 SELECT stock from tradeIndex where stock = 40000;
+### Time: 2.972 ms
 SELECT stock from tradeCluster where stock = 40000;
+### Time: 2.826 ms
 
 ### query to examine the index and cluster index for price and stock
 SELECT price, stock from trade where price >=300 and stock >= 40000;
+### Time: 3867.113 ms
 SELECT price, stock from tradeIndex where price >=300 and stock >= 40000;
+### Time: 4599.691 ms
 SELECT price, stock from tradeCluster where price >=300 and stock >= 40000;
-
+### Time: 4711.858 ms
 
